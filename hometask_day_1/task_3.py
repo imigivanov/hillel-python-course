d91cbb8d3b952f5ea9433569411763406c24813c
Ihor Ivanov <ivanov@partida.io>
2020-04-19 02:46:00 +0300

    CHANGE task_3

--- hometask_day_1/task_3.py
+++ hometask_day_1/task_3.py
@@ -1,12 +1,32 @@
-my_random_string = list("This is just a random string here.")
-my_new_random_string = ""
-vowels = "AEIOUYaeiouy"
-consonants = "BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz"
-for i in range(len(my_random_string)):
-    for j in range(len(vowels)):
-        if my_random_string[i] in vowels[j]:
-            print(my_random_string[i])
-            my_random_string[i] = consonants[j]
-    my_new_random_string += my_random_string[i]
+import random
 
-print(my_new_random_string)
+my_random_string = list("This is just a random string here.")
+vowels_upper = "AEIOUY"
+vowels_lower = "aeiouy"
+consonants_upper = "BCDFGHJKLMNPQRSTVWXZ"
+consonants_lower = "bcdfghjklmnpqrstvwxz"
+
+for i, value in enumerate(my_random_string):
+
+    if value.islower():
+        consonants = consonants_lower
+        vowels = vowels_lower
+    elif value.isupper():
+        consonants = consonants_upper
+        vowels = vowels_upper
+    else:
+        continue
+
+    index = consonants.find(value)
+
+    if index < 0:
+        continue
+
+    if index >= len(vowels):
+        index = random.randint(0, len(vowels) - 1)
+
+    my_random_string[i] = vowels[index]
+
+final_str = ''.join(my_random_string)
+
+print(final_str)
